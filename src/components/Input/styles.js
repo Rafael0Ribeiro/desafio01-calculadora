bd31d48394463a0df8c0ddf24980f4d0ae32de59
Rafael0Ribeiro <149969368+Rafael0Ribeiro@users.noreply.github.com>
2023-11-19 14:12:31 -0300

    Create styles.js

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    height: 75px;
+    width: 100%;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family: 'Roboto';
+
+    input {
+
+    height: 75px;
+    width: 100%;
+    background-color: #AAFFAA;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding:0;
+    font-size:24px;
+    font-family: 'Roboto';
+    padding:10px;
+
+    }
+`
